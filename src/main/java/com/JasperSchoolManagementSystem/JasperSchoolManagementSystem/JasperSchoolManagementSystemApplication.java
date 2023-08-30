f18226a829066f7c5995c2fc0316d3b2235028fd
Lamia <lama5alomairi@gmail.com>
2023-08-30 16:18:32 +0400

    Create JasperSchoolManagementSystem project

---
+++ src/main/java/com/JasperSchoolManagementSystem/JasperSchoolManagementSystem/JasperSchoolManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.JasperSchoolManagementSystem.JasperSchoolManagementSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JasperSchoolManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JasperSchoolManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/JasperSchoolManagementSystem/JasperSchoolManagementSystem/JasperSchoolManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JasperSchoolManagementSystem.JasperSchoolManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JasperSchoolManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
